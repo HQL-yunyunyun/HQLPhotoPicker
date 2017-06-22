237ee89ce84bd103bb4ca9db30f981f17a7e407a
Heqiliang <heqiliang@weplus.cn>
2017-06-22 15:32:13 +0800

    update

---
+++ HQLPhotoPicker/HQLPhotoPicker/HQLPhotoPicker/Helper/HQLPhotoPicker.h
@@ -0,0 +1,17 @@
+//
+//  HQLPhotoPicker.h
+//  HQLPhotoPicker
+//
+//  Created by weplus on 2017/6/22.
+//  Copyright © 2017年 weplus. All rights reserved.
+//
+
+#ifndef HQLPhotoPicker_h
+#define HQLPhotoPicker_h
+
+#import "HQLPhotoManager.h"
+#import "HQLPhotoHelper.h"
+#import "HQLPhotoModel.h"
+#import "HQLPhotoAlbumModel.h"
+
+#endif /* HQLPhotoPicker_h */
